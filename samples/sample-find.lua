d35107c29df7c2bc58f9db21a163e5764d06e48d
Edman Anjos <edmanjos@gmail.com>
2013-08-01 00:52:07 -0300

    Updates sample-find.lua with the new type of return from a find

--- samples/sample-find.lua
+++ samples/sample-find.lua
@@ -1,13 +1,15 @@
 
 pattern {
 	find = true,
-	p = "'a'(?:'b'/'c')'d'",
-	input = { {"zzzzzzabd", {"abd"}} }
+	p = "'a'(?:'b'/'c')'d'*",
+	input = {
+        {"zzzzzzzzzabdd", {{10, 13}}}, {"abd", {{1, 3}}}, {"acd", {{1, 3}}}
+    }
 }
 
 pattern {
 	find = true,
 	p = "[0-9]+",
-	input = { {"this string has 28 characters", {"28"}} }
+	input = { {"this string has 28 characters", {{17, 18}}} }
 }
 
